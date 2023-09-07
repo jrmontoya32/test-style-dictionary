647983a29a8b91f394c304df4b83eb1e58871cc0
Jeremy Montoya <jeremymontoya@Jeremys-MBP.UImbrage.local>
2023-09-07 08:49:22 -0500

    Initial commit

---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Thu, 07 Sep 2023 13:44:54 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
